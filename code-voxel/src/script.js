97601d8af5b0e8db457012d21dcf4dc83c6cbb13
dbf-user <92848627+dbf-user@users.noreply.github.com>
2022-07-07 00:26:19 +0530

    forest added

--- code-voxel/src/script.js
+++ code-voxel/src/script.js
@@ -49,7 +49,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( '#E7C6F8' );
+// scene.background = new THREE.Color( '#E7C6F8' );
 
 
 /**
@@ -103,6 +103,45 @@ hemiLight.position.set(0, 50, 0);
 // Add hemisphere light to scene   
 scene.add(hemiLight);
 
+// Bamboo Forest
+
+// random functions
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
+var myArray = [-45,-40,-35,-30,-25,-20,-15,-10,-5
+  ,5,10,15,20,25,30,35,40,45
+];
+
+function randomItem (){
+ return myArray[Math.floor(Math.random()*myArray.length)];
+}
+
+
+//Create random bamboo forest
+
+
+var loader = new GLTFLoader();
+loader.crossOrigin = true;
+loader.load( 'models/bamboo.glb', function ( data ) {
+
+var count = 25; // getRndInteger(0, 50)
+for(var i = -count; i < count - 1; i++) {
+  
+  var object = data.scene
+  if(count > 1) {
+    object = data.scene.clone();
+  }
+   object.position.set(randomItem(i)-2.5, 0, randomItem(i)-2.5);
+
+  scene.add( object );
+}
+
+});
+
 // Aavatars
 
 // const avatar02 = new GLTFLoader()
@@ -120,23 +159,19 @@ scene.add(hemiLight);
 //     },
 // )
 
-// Models
+// ASSERTS LOAD
 
 const model01 = new GLTFLoader();
 model01.load(
     'models/flat-module.glb',
     function (gltf) {
 
-      const model = gltf.scene;
-      model.position.setX(30);
-      model.position.setZ(70);
-      scene.add(model)
+        const model = gltf.scene;
+        model.position.setX(0);
+        model.position.setZ(70);
+        scene.add(model)
     },
 )
-
-
-
-let glTFGeometry;
 
 const model02 = new GLTFLoader()
 model02.load(
@@ -146,18 +181,6 @@ model02.load(
         const model = gltf.scene;
         model.position.setX(30);
         model.position.setZ(70);
-
-        gltf.scene.traverse( function ( child ) {
-
-          if ( child.isMesh ) {
-
-              //Setting the buffer geometry
-              glTFGeometry = child.geometry;
-
-          }
-
-      } );
-
         scene.add(model)
     },
 )
@@ -168,13 +191,12 @@ model03.load(
     'models/stair-module.glb',
     function (gltf) {
 
-      const model = gltf.scene;
-      model.position.setX(30);
-      model.position.setZ(70);
-      scene.add(model)
+        const model = gltf.scene;
+        model.position.setX(-30);
+        model.position.setZ(70);
+        scene.add(model)
     },
 )
-
 
 
 /**
@@ -206,11 +228,15 @@ window.addEventListener('resize', () =>
 // Base camera
 
 
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-camera.position.set( 50, 80, 130 );
-camera.lookAt( 0, 0, 0 );
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  10000
+);
+camera.position.set(65, 100, 150);
+camera.lookAt(0, 0, 0);
+scene.add(camera);
 
 
 // Controls
@@ -276,119 +302,101 @@ document.addEventListener( 'pointerdown', onPointerDown );
 document.addEventListener( 'keydown', onDocumentKeyDown );
 document.addEventListener( 'keyup', onDocumentKeyUp );
 
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    render();
-
-}
 
 function onPointerMove( event ) {
 
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+  pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
 
-    raycaster.setFromCamera( pointer, camera );
+  raycaster.setFromCamera( pointer, camera );
 
-    const intersects = raycaster.intersectObjects( objects, false );
+  const intersects = raycaster.intersectObjects( objects, false );
 
-    if ( intersects.length > 0 ) {
+  if ( intersects.length > 0 ) {
 
-        const intersect = intersects[ 0 ];
+    const intersect = intersects[ 0 ];
 
-        rollOverMesh.position.copy( intersect.point ).add( intersect.face.normal );
-        rollOverMesh.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+    rollOverMesh.position.copy( intersect.point ).add( intersect.face.normal );
+    rollOverMesh.position.divideScalar( 5 ).floor().multiplyScalar( 5 ).addScalar( 2.5 );
 
-        render();
+    render();
 
-    }
+  }
 
 }
-
-
 
 function onPointerDown( event ) {
 
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+  pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
 
-    raycaster.setFromCamera( pointer, camera );
+  raycaster.setFromCamera( pointer, camera );
 
-    const intersects = raycaster.intersectObjects( objects, false );
+  const intersects = raycaster.intersectObjects( objects, false );
 
-    if ( intersects.length > 0 ) {
+  if ( intersects.length > 0 ) {
 
-        const intersect = intersects[ 0 ];
+    const intersect = intersects[ 0 ];
 
-        // delete cube
+    // delete cube
 
-        if ( event.button == 2 ) {
+    if ( event.button == 2 ) {
 
-            if ( intersect.object !== plane ) {
+      if ( intersect.object !== plane ) {
 
-                scene.remove( intersect.object );
+        scene.remove( intersect.object );
 
-                objects.splice( objects.indexOf( intersect.object ), 1 );
+        objects.splice( objects.indexOf( intersect.object ), 1 );
 
-            }
+      }
 
-            // create cube
-            
+      // create cube
 
-        } else if (event.button == 0) {
+    } if ( event.button == 0 ) {
 
-            const voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
-            voxel.position.copy( intersect.point ).add( intersect.face.normal );
-            voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
-            
-            if (voxel.position.y < 13) {
-              scene.add( voxel );
-              objects.push( voxel );
-            }
+      const voxel = new THREE.Mesh( cubeGeo, new_mtl );
+      voxel.position.copy( intersect.point ).add( intersect.face.normal );
+      voxel.position.divideScalar( 5 ).floor().multiplyScalar( 5 ).addScalar( 2.5 );
 
-            else {
-              alert("Bamboo construction not permitted")
-            }
-
-
-        } else {
-            console.log("Press Mouse Button")
-
-        }
-
-
-        render();
-
+      if (voxel.position.y < 13) {
+        scene.add(voxel);
+        objects.push(voxel);
+      }
+      else {
+        alert("Bamboo construction restricts to 3 storeys")
+      }
+    } else {
+      console.log("Press Mouse Button")
     }
+
+    render();
+
+  }
 
 }
 
-
 function onDocumentKeyDown( event ) {
 
-    switch ( event.keyCode ) {
+  switch ( event.keyCode ) {
 
-        case 16: isShiftDown = true; break;
+    case 16: isShiftDown = true; break;
 
-    }
+  }
 
 }
 
 function onDocumentKeyUp( event ) {
 
-    switch ( event.keyCode ) {
+  switch ( event.keyCode ) {
 
-        case 16: isShiftDown = false; break;
+    case 16: isShiftDown = false; break;
 
-    }
+  }
 
 }
 
 function render() {
 
-    renderer.render( scene, camera );
+  renderer.render( scene, camera );
+
 }
 
 
